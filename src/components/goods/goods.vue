d5294e4bacb71c4a3aebe0ac054b8f37e849385c
adamGAOLH <573301642@.qq.com>
2017-11-13 18:59:31 +0800

    first

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <div>
+    I am goods
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="header">
+  我是 header
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+</style>
