1c862e507a60627399fabf07e0fc99c0fe6d2962
adamGAOLH <573301642@.qq.com>
2017-11-21 15:28:45 +0800

    shopcart

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,147 @@
+<template>
+<div class="shopcart">
+  <div class="content">
+    <div class="content-left">
+      <div class="logo-wrapper">
+        <div class="logo" :class="{'highlight':totalCount>0}">
+          <span class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></span>
+        </div>
+        <div class="num"v-show="totalCount>0">{{totalCount}}</div>
+      </div>
+      <div class="price" :class="{'highlight':totalPrice>0}">
+        {{totalPrice}}
+      </div>
+      <div class="desc">
+        另需配送费￥{{deliveryPrice}}元
+      </div>
+    </div>
+    <div class="content-right">
+      <div class="pay">
+        ￥{{minPrice}}元
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectFoods: {
+      type: Array,
+      default () {
+        return [];
+      }
+    },
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    totalPrice() {
+      let total = 0;
+      this.selectFoods.forEach((food) => {
+        total += food.price * food.count;
+      });
+      return total;
+    },
+    totalCount() {
+      let count = 0;
+      this.selectFoods.forEach((food) => {
+        count += food.count;
+      });
+      return count;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" rel='stylesheet/stylus'>
+  .shopcart
+    position:fixed
+    left:0
+    bottom:0
+    z-index:50
+    width:100%
+    height:48px
+    background:#000
+    .content
+      display:flex
+      background:#141d27
+      color:rgba(255,255,255,0.4)
+      .content-left
+        flex:1
+        .logo-wrapper
+          display:inline-block
+          position:relative
+          top:-10px
+          margin:0 12px
+          padding:6px
+          width:56px
+          height:56px
+          box-sizing:border-box
+          vertical-align:top
+          border-radius:50%
+          background:#141d27
+          .logo
+            width:100%
+            height:100%
+            border-radius:50%
+            background:#2b343c
+            text-align:center
+            &.highlight
+              background:rgb(0,160,220)
+            .icon-shopping_cart
+              color:#80858a
+              line-height:44px
+              font-size:24px
+              &.highlight
+                color:#fff
+          .num
+            position:absolute
+            top:0
+            right:0
+            width:24px
+            height:16px
+            border-radius:16px
+            line-height:16px
+            text-align:center
+            font-size:9px
+            font-weight:700
+            color:#fff
+            background:rgb(240,20,20)
+            box-shadow:0 4px 8px 0 rgba(0,0,0,0.4)
+        .price
+          display:inline-block
+          font-size:16px
+          font-weight:700
+          padding-right:12px
+          vertical-align:top
+          line-height:24px
+          margin-top:12px
+          border-right:1px solid rgba(255,255,255,0.1)
+          &.highlight
+            color:#fff
+        .desc
+          display:inline-block
+          vertical-align:top
+          line-height:24px
+          margin:12px 0 0 12px
+          font-size:10px
+      .content-right
+        flex:0 0 105px
+        width:105px
+        .pay
+          font-size:12px
+          line-height:48px
+          height:48px
+          text-align:center
+          background:#2b333b
+
+
+</style>
